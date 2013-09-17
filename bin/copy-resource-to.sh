8691b169843f457ce0e3b1b8469f7a10765751e0
Tom Bentley <tom.bentley@cantab.net>
2013-09-17 12:41:48 +0100

    Remove unused scripts. Fixes #159

--- bin/copy-resource-to.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]
-then
-    echo "copy-resource-to.sh <root-of -eylon-lang.org>"
-else 
-    rsync -avz --delete --progress ../_site/documentation/1.0/spec/html ../_site/documentation/1.0/spec/html_single ../_site/documentation/1.0/spec/pdf ../_site/documentation/1.0/spec/shared $1/documentation/1.0/spec
-    rsync -avz --delete --progress ../_site/documentation/1.0/api $1/documentation/1.0/api
-fi
