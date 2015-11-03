1ecd9ff9a467ca11bbec6e7c7cf3b7b5ac117679
gavinking <gavin@ceylon-lang.org>
2015-11-03 02:29:32 +0100

    use new URL for embedded

--- js/common.js
+++ js/common.js
@@ -142,7 +142,7 @@ function addTryButtons(){
 					// Chrome would be "chromium-browser --disable-web-security" for example
 					if(document.domain != "localhost")
 						document.domain = "ceylon-lang.org";
-					$editorIFrame = jQuery("<iframe class='code-editor' src='http://try.ceylon-lang.org/embedded.html'>");
+					$editorIFrame = jQuery("<iframe class='code-editor' src='http://try.ceylon-lang.org/index.html?embedded=true'>");
 					$editorIFrame.load(function(){
 						updateEditor(src);
 					});
