bb767722e629adafc29b3f74afa332e742acf07a
Stephane Epardaud <stef@epardaud.fr>
2016-09-12 15:46:57 +0200

    Fix remaining HTTP URLs

--- _ext/mydisqus.rb
+++ _ext/mydisqus.rb
@@ -17,11 +17,11 @@ module Awestruct
             #{developer}
             (function() {
               var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;
-              dsq.src = "http://#{site.disqus}.disqus.com/embed.js";
+              dsq.src = "//#{site.disqus}.disqus.com/embed.js";
               (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
             })();
             </script>
-            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=#{site.disqus}">comments powered by Disqus.</a></noscript>
+            <noscript>Please enable JavaScript to view the <a href="//disqus.com/?ref_noscript=#{site.disqus}">comments powered by Disqus.</a></noscript>
           }
         end
 
@@ -35,7 +35,7 @@ module Awestruct
             var disqus_shortname = '#{site.disqus}';
             (function () {
               var s = document.createElement('script'); s.async = true;
-              s.src = "http://disqus.com/forums/#{site.disqus}/count.js";
+              s.src = "//disqus.com/forums/#{site.disqus}/count.js";
               (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
             }());
             </script>
